8ec3e4c44e13da150023b7009c202f261727c7e5
alexkernphysiker <kernphysiker@openmailbox.org>
2016-03-02 15:43:05 +0100

    cosmetic fix

--- Preselection/data.cc
+++ Preselection/data.cc
@@ -7,9 +7,10 @@
 #include "reconstruction.h"
 using namespace std;
 const double pbeam_measurement_offset=0.007;
-RealData::RealData():BeamMomenta("Time.2.PBeam",[this](){
-	return 1000.0*fHeader->GetTimeInCycle();
-},[](){return INFINITY;}){
+RealData::RealData():BeamMomenta("Time.2.PBeam",
+	[this](){return 1000.0*fHeader->GetTimeInCycle();},
+	[](){return INFINITY;}
+){
 	AddLogSubprefix("Real data analysis");
 	fHeader = dynamic_cast<REventHeader*>(gDataManager->GetDataObject("REventHeader","Header"));
 }
