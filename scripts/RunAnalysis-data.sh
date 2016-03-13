be03b18902d0bf79c84ab2b6186fcf4ae574ce3b
alexkernphysiker <kernphysiker@openmailbox.org>
2016-03-13 15:26:40 +0100

    Project dir. renamed

--- scripts/RunAnalysis-data.sh
+++ scripts/RunAnalysis-data.sh
@@ -9,7 +9,7 @@ for X in `seq 45873 1 46884`; do
 					echo "#PBS -N DATA_${X}_$1" >> ${scriptname}
 					echo "#PBS -l walltime=48:00:00" >> ${scriptname}
 					echo >> ${scriptname}
-					echo "cd $PWD/../Preselection" >> ${scriptname}
+					echo "cd $PWD/../Sorting" >> ${scriptname}
 					echo "./main Data_$1 -fin cluster:${RUNS_DATA}/run_${X} -r ${X} -n Data$1_run_${X} -lf run_${X}.log -abort " >> ${scriptname}
 					echo >> ${scriptname}
 					echo "rm -f $PWD/${scriptname}" >> ${scriptname}
--- scripts/RunReconstructionSimulation.sh
+++ scripts/RunReconstructionSimulation.sh
@@ -4,7 +4,7 @@ echo "#!/bin/bash" >> ${scriptname}
 echo "#PBS -N RE_$1" >> ${scriptname}
 echo "#PBS -l walltime=48:00:00" >> ${scriptname}
 echo >> ${scriptname}
-echo "cd $PWD/../Preselection" >> ${scriptname}
+echo "cd $PWD/../Sorting" >> ${scriptname}
 echo "./main RE_$1 -mode mc -fin file:$WMC_DATA/$1.wmc.data -n RE$1 -abort" >> ${scriptname}
 echo >> ${scriptname}
 echo "rm -f $PWD/${scriptname}" >> ${scriptname}
