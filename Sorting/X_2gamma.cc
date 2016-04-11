f9fdcc1d35272013d5c85ec5484ccfd0582b3d33
alexkernphysiker <kernphysiker@openmailbox.org>
2016-04-11 19:08:27 +0200

    invariant mass fix

--- Sorting/X_2gamma.cc
+++ Sorting/X_2gamma.cc
@@ -17,12 +17,11 @@ namespace ReactionSetup{
 		}
 		);
 		auto im_val=make_shared<double>(INFINITY);
-		res.Trigger(17).post()
-		<< [data,im_val](){
-			(*im_val)=INFINITY;
-			SortedPoints<double> table;
-			for(size_t i=0;i<data->size();i++)
-				for(size_t j=i+1;j<data->size();j++){
+		res.Trigger(17).post() << (make_shared<ChainCheck>()
+			<< [data,im_val](){
+				(*im_val)=INFINITY;
+				SortedPoints<double> table;
+				for(size_t i=0;i<data->size();i++)for(size_t j=i+1;j<data->size();j++){
 					double im=InvariantMass({data->operator[](i),data->operator[](j)});
 					double rest=0;
 					for(size_t k=0;k<data->size();k++)if((k!=i)&&(k!=j))
@@ -30,11 +29,14 @@ namespace ReactionSetup{
 					table<<point<double>(rest,im);
 				}
 				if(table.size()>0)
-					if(table[0].X()<0.050)//noise threshold
+					if(table[0].X()<0.050){//noise threshold
 						(*im_val)=table[0].Y();
-					return true;
-		}
-		<< make_shared<SetOfHists1D>("CentralGammas","InvMass2Gamma",Q_axis(res),Axis([im_val]()->double{return *im_val;},0.0,0.8,800))
+						return true;
+					}
+				return false;
+			}
+			<< make_shared<SetOfHists1D>("CentralGammas","InvMass2Gamma",Q_axis(res),Axis([im_val]()->double{return *im_val;},0.0,0.8,800))
+		)
 		<< make_shared<Hist1D>("CentralGammas","neutral_tracks_count",Axis([data]()->double{return data->size();},-0.5,9.5,10));
 	}
 	
