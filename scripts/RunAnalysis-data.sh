98b96d6a0609aafc57db81de45dc56f8ca208683
alexkernphysiker <kernphysiker@openmailbox.org>
2016-04-12 16:21:30 +0200

    script fix

--- scripts/RunAnalysis-data.sh
+++ scripts/RunAnalysis-data.sh
@@ -1,7 +1,7 @@
 for X in `seq 45873 1 46884`; do
 	if [ -f ${RUNS_DATA}/run_${X} ]; then
 		if [ ! -f $PWD/../Sorting/Data_run_${X}.root ]; then
-			if [ `qstat|grep DATA|grep "R \|Q "|wc -l` -lt 50 ]; then
+			if [ `qstat|grep "R \|Q "|wc -l` -lt 75 ]; then
 				if [ `qstat|grep ${X}_|wc -l` -lt 1 ]; then
 					scriptname="run_${X}.sh"
 					rm -f ${scriptname}
