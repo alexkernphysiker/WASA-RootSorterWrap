b2d7c02d62eda5079f4d86d84bcd091ef4cbca45
rundel@wasa00 <kernphysiker@openmailbox.org>
2016-04-12 16:16:59 +0200

    script fix

--- scripts/clear_tasks.sh
+++ scripts/clear_tasks.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for X in `qstat | grep DATA_|cut -c1-7`; do echo $X; qdel $X; sleep 2; done
+for X in `qstat | grep "R \|Q " |cut -c1-7`; do echo $X; qdel $X; sleep 2; done
 rm run*.sh
 rm *.o* *.e*
 rm ../Sorting/*.log ../Sorting/*.root
