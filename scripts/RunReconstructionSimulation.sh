d94d5b5fc880b2bec513a9ef1672663b409b208b
alexkernphysiker <kernphysiker@openmailbox.org>
2016-12-16 15:01:37 +0100

    WMC works with many files for one reaction

--- scripts/RunReconstructionSimulation.sh
+++ scripts/RunReconstructionSimulation.sh
@@ -1,15 +1,16 @@
-scriptname="run_mc.sh"
-rm -f ${scriptname}
-echo "#!/bin/bash" >> ${scriptname}
-echo "#PBS -N RE_$1" >> ${scriptname}
-echo "#PBS -l walltime=48:00:00" >> ${scriptname}
-echo >> ${scriptname}
-echo "cd $PWD/../Sorting" >> ${scriptname}
-echo "./main RE_$1 -mode mc -fin file:$WMC_DATA/$1.wmc.data -n RE$1 -abort" >> ${scriptname}
-echo >> ${scriptname}
-echo "rm -f $PWD/${scriptname}" >> ${scriptname}
-chmod u+x ${scriptname}
-qsub ${scriptname}
-echo "${scriptname} generated and executed"
+for X in `seq 1 1 20`; do
+    scriptname="run_reconstr-$X.sh"
+    rm -f ${scriptname}
+    echo "#!/bin/bash" >> ${scriptname}
+    echo "#PBS -N RE_$1-$X" >> ${scriptname}
+    echo "#PBS -l walltime=48:00:00" >> ${scriptname}
+    echo >> ${scriptname}
+    echo "cd $PWD/../Sorting" >> ${scriptname}
+    echo "./main RE_$1-$X -mode mc -fin file:$WMC_DATA/$1-$X.wmc.data -n RE$1-$X -abort" >> ${scriptname}
+    echo >> ${scriptname}
+    echo "rm -f $PWD/${scriptname}" >> ${scriptname}
+    chmod u+x ${scriptname}
+    qsub ${scriptname}
+    echo "${scriptname} generated and executed"
+done
 qstat
-
--- scripts/TestAnalysis-mc.sh
+++ scripts/TestAnalysis-mc.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 cd ../Sorting
-./main MC_$1 -mode mc -fin file:$WMC_DATA/$1.wmc.data -n MC$1 -abort
-
+./main MC_$1-1 -mode mc -fin file:$WMC_DATA/$1-1.wmc.data -n MC$1-1 -abort
