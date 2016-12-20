47633db8db36264634e724b96c04ecfc092aef46
rundel@wasa00 <kernphysiker@openmailbox.org>
2016-12-20 14:34:56 +0100

    WMC script upgrade

--- scripts/QueueWMC.sh
+++ scripts/QueueWMC.sh
@@ -5,15 +5,19 @@ for X in `seq 1 1 100`; do
 	    echo "was already done"
 	else
 	    scriptname="run_wmc-$1-$X.sh"
+	    dirname="wmc-$1-$X"
+	    mkdir ${dirname}
+	    cp WMC/* ${dirname}
 	    rm -f ${scriptname}
 	    echo "#!/bin/bash" >> ${scriptname}
 	    echo "#PBS -N WMC_$1-$X" >> ${scriptname}
 	    echo "#PBS -l walltime=48:00:00" >> ${scriptname}
 	    echo >> ${scriptname}
-	    echo "cd $PWD/WMC" >> ${scriptname}
+	    echo "cd $PWD/${dirname}" >> ${scriptname}
 	    echo "./wmc.sh $PLUTO_OUTPUT/$1-$X.root $WMC_DATA/$1-$X.wmc.data" >> ${scriptname}
 	    echo >> ${scriptname}
 	    echo "rm -f $PWD/${scriptname}" >> ${scriptname}
+	    echo "rm -rf $PWD/${dirname}" >> ${scriptname}
 	    chmod u+x ${scriptname}
 	    qsub ${scriptname}
 	    echo "${scriptname} generated and executed"
