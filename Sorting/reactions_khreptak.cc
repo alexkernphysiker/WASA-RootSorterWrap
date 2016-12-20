fc77b6d6c0122d5639c15571370316db639336ea
alexkernphysiker <kernphysiker@openmailbox.org>
2016-12-20 17:11:10 +0100

    fix stage 2

--- Sorting/reactions_khreptak.cc
+++ Sorting/reactions_khreptak.cc
@@ -1,15 +1,12 @@
 #include <Wasa.hh>
 #include <TCutG.h>
 #include <vector>
-#include "math_h/functions.h"
+#include <memory>
 #include "math_h/functions.h"
 #include "math_h/vectors.h"
-#include "ReconstructionFit/reconstruction_types.h"
 #include "Experiment/experiment_conv.h"
 #include "Kinematics/reactions.h"
 #include "trackprocessing.h"
-#include "detectors.h"
-#include "reconstruction.h"
 #include "data.h"
 
 #include "reactions_khreptak.h"
@@ -23,7 +20,7 @@ namespace Khreptak{
     }
     Axis Q_axis(const Analysis&res){return Axis([&res]()->double{return 1000.0*He3eta().P2Q(res.PBeam());},-70.0,30.0,40);}
     void SetupForPPNsp(Analysis&res){
-	        auto count_ch_CD=make_shared<long>(0);
+	auto count_ch_CD=make_shared<long>(0);
         auto count_ch_FD=make_shared<long>(0);
         auto count_compl=make_shared<long>(0);
 
@@ -46,15 +43,13 @@ namespace Khreptak{
 		if (T.Type()==kFDC)
 		    (*count_ch_FD)++;
 		if ((T.Type()==kCDC) || (T.Type()==kFDC)){
-		    Theta->push_back(T.Theta());
-		    Phi->push_back(T.Phi());
+		    Theta->push_back(T.Theta());//!!!!
+		    Phi->push_back(T.Phi());//!!!!
 		}
 		return true;
 	    };
 
         res.Trigger(0).post()
-	    <<make_shared<Hist1D>("ppn","ChargedCentral",Axis([count_ch_CD]()->double{return *count_ch_CD;},-0.5,9.5,10))
-	    <<make_shared<Hist1D>("ppn","ChargedForward",Axis([count_ch_FD]()->double{return *count_ch_FD;},-0.5,9.5,10))
 	    <<[Theta, Phi,count_compl](){
 		(*count_compl)=0;
 		vector<Vector3<double>> P;
@@ -63,14 +58,16 @@ namespace Khreptak{
 		}
 		for(size_t i=0; i<P.size(); i++){
 		    for(size_t j=i+1; j<P.size(); j++){
-			auto B=Vector3<double>::basis_z();
-			double compl=B.VecP(P[i])*P[j];
+			const double compl=(Vector3<double>::basis_z().VecP(P[i])*P[j]);
 			if(pow(compl,2)<0.1){
 			    (*count_compl)++;
 			}
 		    }
 		}
+		return true;//!!!!!!
 	    }
+	    <<make_shared<Hist1D>("ppn","ChargedCentral",Axis([count_ch_CD]()->double{return *count_ch_CD;},-0.5,9.5,10))
+	    <<make_shared<Hist1D>("ppn","ChargedForward",Axis([count_ch_FD]()->double{return *count_ch_FD;},-0.5,9.5,10))
 	    <<make_shared<Hist1D>("ppn","Complanarity",Axis([count_compl]()->double{return *count_compl;},-0.5,9.5,10));
     }
 }
