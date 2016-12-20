6ab9446f329e671b22893845a45f6037c4864739
alexkernphysiker <kernphysiker@openmailbox.org>
2016-12-20 13:28:38 +0100

    example

---
+++ Sorting/example.h
@@ -0,0 +1,9 @@
+// this file is distributed under 
+// MIT license
+#ifndef _____EXAMPLE_HEADER_______
+#define _____EXAMPLE_HEADER_______
+#include "prepare.h"
+namespace ReactionSetup{
+    void prepare_example_analyse(Analysis&res);
+}
+#endif 
--- Sorting/main.cc
+++ Sorting/main.cc
@@ -9,47 +9,54 @@
 #include "prepare.h"
 #include "he3_X.h"
 #include "X_2gamma.h"
+#include "example.h"
 using namespace std;
 using namespace MathTemplates;
 using namespace ReactionSetup;
 int main(int argc, char** argv) {
-	if(argc<2)
-		return -1;
-	int new_c=argc-1;
-	char *args[new_c+1];
-	args[0]=argv[0];
-	string type(argv[1]);
-	SetAnalysisType([type](){
-		Analysis* res=nullptr;
-		if("Data"==type)
-			res=new RealData();
-		else
-			res=new MonteCarlo(true);
-		if(
-			("RE_He3eta"==type)||
-			("RE_He3pi0"==type)||
-			("RE_He3pi0pi0"==type)||
-			("RE_He3pi0pi0pi0"==type)
-		){
-			He3_X_reconstruction(*res);;
-		}
-		if(
-			("Data"==type)||
-			("MC_He3eta"==type)||
-			("MC_He3pi0"==type)||
-			("MC_He3pi0pi0"==type)||
-			("MC_He3pi0pi0pi0"==type)
-		){
-			He3_X_analyse(*res);
-			SearchGammaTracks(*res);
-		}
-		return res;
-	});
-	for(int i=1;i<=new_c;i++)
-		args[i]=argv[i+1];
-	gSorterConfig->ReadCmdLine(new_c,args);
-	Wasa::Initialize("AnalysisModule","","RootSorter.log");
-	gWasa->AddAnalysis("AnalysisModule","Raw");
-	gWasa->Run();
-	delete gWasa;
+    if(argc<2)
+	return -1;
+    int new_c=argc-1;
+    char *args[new_c+1];
+    args[0]=argv[0];
+    string type(argv[1]);
+    SetAnalysisType([type](){
+	Analysis* res=nullptr;
+	if("Data"==type)
+	    res=new RealData();
+	else
+	    res=new MonteCarlo(true);
+	if(
+	    ("RE_He3eta"==type)||
+	    ("RE_He3pi0"==type)||
+	    ("RE_He3pi0pi0"==type)||
+	    ("RE_He3pi0pi0pi0"==type)
+	){
+	    He3_X_reconstruction(*res);;
+	}
+	if(
+	    ("Data"==type)||
+	    ("MC_He3eta"==type)||
+	    ("MC_He3pi0"==type)||
+	    ("MC_He3pi0pi0"==type)||
+	    ("MC_He3pi0pi0pi0"==type)
+	){
+	    He3_X_analyse(*res);
+	    SearchGammaTracks(*res);
+	}
+	if(
+	    ("MC_EXAMPLE"==type)||
+	    ("Data_EXAMPLE"==type)
+	){
+	    prepare_example_analyse(*res);;
+	}
+	return res;
+    });
+    for(int i=1;i<=new_c;i++)
+	args[i]=argv[i+1];
+    gSorterConfig->ReadCmdLine(new_c,args);
+    Wasa::Initialize("AnalysisModule","","RootSorter.log");
+    gWasa->AddAnalysis("AnalysisModule","Raw");
+    gWasa->Run();
+    delete gWasa;
 }
